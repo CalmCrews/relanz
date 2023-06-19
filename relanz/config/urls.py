bcaee227a758790831a2527e6d3681272e8a5c69
imjanghyeok <qkdrhkgn23@naver.com>
2023-06-19 15:05:12 +0900

    setting:main 만들기 및 오류 수정

--- relanz/config/urls.py
+++ relanz/config/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('main.urls')),
     path('user/', include('user.urls')),
     path('account/', include('account.urls')),
     path('community/', include('community.urls')),
