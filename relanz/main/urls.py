2ed39bfeb05ec64b9648831bbdf6875216eea98a
imjanghyeok <qkdrhkgn23@naver.com>
2023-06-20 12:09:20 +0900

    start-home 오류 수정

--- relanz/main/urls.py
+++ relanz/main/urls.py
@@ -7,5 +7,6 @@ app_name = 'main'
 urlpatterns = [
     path('', views.home, name="home"),
     # 이인이 signin.html 연결해볼라고 임시로 만듬
-    path('login/', views.login, name="login"),
+    # path('login/', views.login, name="login"), 
+    # 주석 처리 할게요
 ]
