1e7600827971071654f915a6bb8ac5b5543e0be5
LinQuixote <leein9404@naver.com>
2023-06-20 16:55:12 +0900

    style/signup.html

---
+++ relanz/static/js/signup.js
@@ -0,0 +1,26 @@
+console.log("hih")
+const onClickCheckId = async () => {
+    const id = document.getElementById("username").value
+    if (!id) {
+        // 빈 문자열일경우. 혹시 문자열의 형식을 정하면 여기에 정규식 표현으로 필터링도 가능 & 유저 피드백 처리도
+        return
+    }
+    // 여기에다 url을 알려줘~
+    const url =``
+    const response = await fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            id,
+          }),
+    })
+
+    // 여기에서 백에서 준 데이터 받아 변수에 할당하는 곳!
+    const {} = await response.json()
+
+    // 이후 처리에 따라 아이디 중복 여부를 유저에게 피드백하면 됨!
+}
+
+document.getElementById("username_button").addEventListener("click", onClickCheckId)
