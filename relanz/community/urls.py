451472e100bb2b5fb666aeef138d8e7979f894c6
tmdtmdqorekf <tmdtmdqorekf@naver.com>
2023-06-21 15:31:31 +0900

    feature: community 기록게시판 메인페이지 구현

--- relanz/community/urls.py
+++ relanz/community/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import new, detail, edit, delete
+from .views import new, detail, edit, delete, communityHome
 
 app_name = 'community'
 
 urlpatterns = [
+    path('communityHome/', communityHome, name='communityHome'),
     path('new/', new, name='new'),
     path('<int:article_id>', detail, name='detail'),
     path('edit/<int:article_id>', edit, name='edit'),
