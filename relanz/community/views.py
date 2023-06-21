2358be4af0bb4bf067e1d9a78c1bcd916c36104d
imjanghyeok <qkdrhkgn23@naver.com>
2023-06-21 18:28:24 +0900

    fix: login안할 시 게시판 이용불가

--- relanz/community/views.py
+++ relanz/community/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import ArticleForm
 from .models import Article
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+@login_required(login_url='/user/signin')
 def communityHome(request):
     articles = Article.objects.all()
     return render(request, 'community/communityHome.html', {'articles':articles})
 
+@login_required(login_url='/user/signin')
 def new(request):
     form = ArticleForm()
 
@@ -22,11 +25,13 @@ def new(request):
 
     return render(request, 'community/new.html', {'form':form})
 
+@login_required(login_url='/user/signin')
 def detail(request, article_id):
     article = get_object_or_404(Article, pk=article_id)
 
     return render(request, 'community/detail.html', {'article':article})
 
+@login_required(login_url='/user/signin')
 def edit(request, article_id):
     article = get_object_or_404(Article, pk=article_id)
 
@@ -41,6 +46,7 @@ def edit(request, article_id):
     
     return render(request, 'community/edit.html', {'form':form, 'article':article})
 
+@login_required(login_url='/user/signin')
 def delete(request, article_id):
     article = get_object_or_404(Article, pk=article_id)
 
