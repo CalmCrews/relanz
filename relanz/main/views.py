0a0f8ccffb9b945bf06cab739a7a23af47dd3cd7
Woorim Kim <wrkimlimi@gmail.com>
2023-06-21 17:59:03 +0900

    fix: splashscreen 스크립트 수정

--- relanz/main/views.py
+++ relanz/main/views.py
@@ -2,17 +2,15 @@ from django.shortcuts import render, redirect
 from user.models import User
 
 # Create your views here.
+def home(request):
+    user=request.user
+    if request.user.is_authenticated:
+        return render(request, 'main/home.html', {'user':user})
+    return render(request, 'main/home.html')
+
 
 def welcome(request):
     if request.user.is_anonymous:
         return render(request, 'main/splashscreen.html')
     else:
         return redirect('main:home')
-
-
-
-def home(request):
-    user=request.user
-    if request.user.is_authenticated:
-        return render(request, 'main/home.html', {'user':user})
-    return render(request, 'main/home.html')
