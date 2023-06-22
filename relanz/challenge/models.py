6a859af2792f36dffd3d04d5a2f23953f8bdc822
imjanghyeok <qkdrhkgn23@naver.com>
2023-06-23 03:22:52 +0900

    feature: challenge-model 생성

--- relanz/challenge/models.py
+++ relanz/challenge/models.py
@@ -1,25 +1,12 @@
 from django.db import models
 
 # Create your models here.
-class challenge():
+class Challenge(models.Model):
     class Meta:
         db_table="challenge"
-    title_tuple = [
-        ('climbing', '클라이밍'),
-        ('walking', '산책하기')
-    ]
 
-    simple_tuple = [
-        ()
-    ]
-
-    effect_tuple = [
-        ()
-    ]
-
-    title = models.CharField(max_length=20, choices=title_tuple, blank=False)
-    simple_effect = models.CharField(max_length=20, choices=title_tuple, blank=False)
-    effect = models.CharField(max_length=20, choices=title_tuple, blank=False)
-    recommnad = models.CharField(max_length=20, choices=title_tuple, blank=False)
-    image = models.ImageField(upload_to='appname')
-    
+    title = models.CharField(max_length=32, unique=True, blank=False)
+    sub_effect = models.TextField(blank=False)
+    effect = models.TextField(blank=False)
+    recommnad = models.TextField(blank=False)
+    image = models.ImageField(upload_to='images/challenge', default='images/challenge/Tom.gif')
