e4d1b4bec3e6f7c65c150df8212d6292f264911b
tmdtmdqorekf <tmdtmdqorekf@naver.com>
2023-06-25 02:43:26 +0900

    feature: 비밀번호 초기화, 재설정 구현

--- relanz/user/views/find_views.py
+++ relanz/user/views/find_views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404, resolve_url
 from ..models import User
 from django.core.mail import send_mail
-
+import os
+    
 def findid(request):
     if request.method == 'POST':
         email = request.POST['email']
@@ -9,14 +10,15 @@ def findid(request):
             user = User.objects.get(email=email)
             subject = f'회원님의 아이디는 {user.username}입니다.'
             message = f"회원님의 아이디는 {user.username}입니다. 이 메일은 회신불가입니다."
-            from_email = 'relanz.noreply@gmail.com'
+            from_email = os.environ.get('EMAIL_HOST_USER')
             recipient_list = [email]
 
             send_mail(subject=subject, message=message, from_email=from_email, recipient_list=recipient_list)
 
-            return render(request, 'user/signin.html', {'success': True})
+            return redirect('user:signin')
         
         except User.DoesNotExist:
             return render(request, 'user/findid.html', {'error': '일치하는 사용자가 없습니다.'})
 
     return render(request, 'user/findid.html')
+
