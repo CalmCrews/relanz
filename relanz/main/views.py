9e4aeff628f6835fa9d9d9ec61f3551ce282c717
imjanghyeok <qkdrhkgn23@naver.com>
2023-06-25 01:17:38 +0900

    fix:  migration

--- relanz/main/views.py
+++ relanz/main/views.py
@@ -6,24 +6,26 @@ from challenge.models import Challenge, ChallengeTag, Participant
 def home(request):
     user=request.user
     if request.user.is_authenticated:
-        user_tag = UserTag.objects.get(user=user.id)
-        basic_tags = {
-        'morning': user_tag.morning,
-        'afternoon': user_tag.afternoon,
-        'evening': user_tag.evening,
-        'inside': user_tag.inside,
-        'outside': user_tag.outside,
-        'solo': user_tag.solo,
-        'group': user_tag.group,
-        'extreme': user_tag.pay,
-        'calm': user_tag.free,
-        'focus': user_tag.static,
-        'achievement': user_tag.dynamic,
-        }
-        user_tag_list=[]
-        for tag_name, tag_value in basic_tags.items():
-            if tag_value is True:
-                user_tag_list.append(tag_name)
-
-        return render(request, 'main/home.html', {'user':user})
+        try:
+            user_tag = UserTag.objects.get(user=user.id)
+            basic_tags = {
+            'morning': user_tag.morning,
+            'afternoon': user_tag.afternoon,
+            'evening': user_tag.evening,
+            'inside': user_tag.inside,
+            'outside': user_tag.outside,
+            'solo': user_tag.solo,
+            'group': user_tag.group,
+            'extreme': user_tag.pay,
+            'calm': user_tag.free,
+            'focus': user_tag.static,
+            'achievement': user_tag.dynamic,
+            }
+            user_tag_list=[]
+            for tag_name, tag_value in basic_tags.items():
+                if tag_value is True:
+                    user_tag_list.append(tag_name)
+            return render(request, 'main/home.html', {'user':user})
+        except UserTag.DoesNotExist:
+            return render(request, 'main/home.html', {'user':user})
     return render(request, 'main/splashscreen.html')
