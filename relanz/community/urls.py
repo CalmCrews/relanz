054e8aec92bc332ac3e37ab3d05644df19e3fa81
tmdtmdqorekf <tmdtmdqorekf@naver.com>
2023-06-28 19:36:57 +0900

    fix: community 챌린지 id에 맞게 연결 링크 수정

--- relanz/community/urls.py
+++ relanz/community/urls.py
@@ -4,10 +4,10 @@ from .views import new, detail, edit, delete, communityHome, like
 app_name = 'community'
 
 urlpatterns = [
-    path('communityHome/', communityHome, name='communityHome'),
-    path('new/', new, name='new'),
-    path('<int:article_id>', detail, name='detail'),
-    path('edit/<int:article_id>', edit, name='edit'),
-    path('delete/<int:article_id>', delete, name='delete'),
+    path('communityHome/<int:challenge_id>', communityHome, name='communityHome'),
+    path('<int:challenge_id>/<int:article_id>', detail, name='detail'),
+    path('<int:challenge_id>/new', new, name='new'),
+    path('edit/<int:challenge_id>/<int:article_id>', edit, name='edit'),
+    path('delete/<int:challenge_id>/<int:article_id>', delete, name='delete'),
     path('<int:article_id>/like/', like, name='like')
 ]
