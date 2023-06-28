6317a4ab68cd0e46d246f6e6e2ce083762239876
tmdtmdqorekf <tmdtmdqorekf@naver.com>
2023-06-28 16:57:02 +0900

    fix: community/models.py author 연결 수정

--- relanz/community/models.py
+++ relanz/community/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from user.models import User
+from challenge.models import Participant
 
 # Create your models here.
 
 class Article(models.Model):
-    author = models.ForeignKey('user.User', on_delete=models.CASCADE)
+    author = models.ForeignKey(Participant, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, default=0, on_delete=models.CASCADE, related_name='articles') # Participant가 작성한 글 유저와 연결 위해 추가
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     image = models.ImageField(blank=True, null=True, upload_to='')
--- relanz/community/views.py
+++ relanz/community/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.core.paginator import Paginator
 from .forms import ArticleForm
-from .models import Article, Like
+from .models import Article, Like, Participant
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 
 # Create your views here.
 
