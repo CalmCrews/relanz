c26c99aa42d4bdc7f41ad6bc94c440701b52bcf0
LinQuixote <leein9404@naver.com>
2023-06-30 23:12:40 +0900

    fix:tagsurvey 해결

--- relanz/static/js/tagsurvey.js
+++ relanz/static/js/tagsurvey.js
@@ -55,7 +55,7 @@ function selectMyTag_ver2 (obj) {
     const tag = document.getElementById(inputId)
 
     const morning = document.getElementById("type_second_morning")
-    const nooing = document.getElementById("type_second_nooing")
+    const nooing = document.getElementById("type_second_afternoon")
     const evening = document.getElementById("type_second_evening")
     const anytime = document.getElementById("type_second_anytime")
 
@@ -100,3 +100,44 @@ function selectMyTag_ver2 (obj) {
         
     }
 }
+
+
+window.addEventListener("load", () => {
+    const tag_lists = ["morning", "afternoon", "evening", "inside", "outside", "solo", "group", "static", "dynamic"];
+    const isClickedAboutTime = [false, false, false];
+    let handleTime;
+    for (let i=0; i<tag_lists.length; i++) {
+        const id = `tags_list_${tag_lists[i]}`;
+        const divId = tag_lists[i] === "morning" ||tag_lists[i] === "afternoon"|| tag_lists[i] === "evening" ? `type_second_${tag_lists[i]}` : `type_third_${tag_lists[i]}`
+        const isClicked = document.getElementById(id).dataset.tags === "True";
+        console.log(i, divId, isClicked)
+        if (isClicked) {
+            if (divId.includes("third")) {
+                document.getElementById(divId).click();
+                console.log(document.getElementById(divId));
+            } else {
+                switch (tag_lists[i]) {
+                    case "morning":
+                        isClickedAboutTime[0] = true;
+                        break;
+                    case "afternoon":
+                        isClickedAboutTime[1] = true;
+                        break;
+                    case "evening":
+                        isClickedAboutTime[2] = true;
+                        handleTime = String(isClickedAboutTime.filter((boolean)=>boolean).length) === "3";
+                        break;
+                }
+            }
+        }
+        if (isClickedAboutTime.length === 3) {
+            document.getElementById("type_second_anytime").click();
+        } else {
+            isClickedAboutTime.forEach((value, index)=>{
+                if (Boolean(value)) {
+                    document.getElementById(`type_second_${tag_lists[index]}`).click();
+                }
+            })
+        }        
+    }
+});
