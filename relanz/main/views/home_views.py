3da38c172c2d6cefdb02aec7b10c917cc8712468
tmdtmdqorekf <tmdtmdqorekf@naver.com>
2023-07-03 16:26:28 +0900

    fix: survey result 예외처리 한 곳에 누락된거 처리

--- relanz/main/views/home_views.py
+++ relanz/main/views/home_views.py
@@ -57,7 +57,12 @@ def home(request):
                 for tag_list in tag_lists:
                     challenge_query |= Q(**{tag_list: True})
                 challenges = ChallengeTag.objects.filter(challenge_query)
-                return render(request, 'main/home.html', {'user':user, 'tag_lists':tag_lists_, 'challengs':challenges})
+
+                res_data = survey_result(request)
+                res_data2 = {'user':user, 'tag_lists':tag_lists_, 'challengs':challenges}
+                res_data.update(res_data2)
+
+                return render(request, 'main/home.html', res_data)
             else:
                 user_tag = UserTag.objects.get(user=user.id)
                 basic_tags_ = {
