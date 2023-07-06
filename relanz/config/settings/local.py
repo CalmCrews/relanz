c08d8167d795f9428185c0f4924dfcd7b0517e25
imjanghyeok <qkdrhkgn23@naver.com>
2023-07-06 12:56:49 +0900

    deploy: first

---
+++ relanz/config/settings/local.py
@@ -0,0 +1,12 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ["*"]
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
