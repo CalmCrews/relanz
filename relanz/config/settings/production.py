e39a7a4e916d4462a205dcd276c5ec6aaf4bbb1a
imjanghyeok <qkdrhkgn23@naver.com>
2023-07-06 13:10:00 +0900

    deploy: first-탄력적 IP 할당

--- relanz/config/settings/production.py
+++ relanz/config/settings/production.py
@@ -3,7 +3,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["52.78.70.54"]
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
