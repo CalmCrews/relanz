8edd3c46e76cc02a772bf3a8727bb033daa381a9
LinQuixote <leein9404@naver.com>
2023-07-07 14:34:38 +0900

    style: 해결

--- relanz/static/js/challenge.js
+++ relanz/static/js/challenge.js
@@ -1,1 +1,27 @@
-document.getElementById(`${color}_${num}`).src = ``
+// document.getElementById(`${color}_${num}`).src = ``
+
+
+const contentDiv_1 = document.getElementById("linebreaks_fix_1");
+const contentDiv_2 = document.getElementById("linebreaks_fix_2");
+
+const paragraphs_1 = contentDiv_1.innerHTML.split("<br>");
+const paragraphs_2 = contentDiv_2.innerHTML.split("<br>");
+contentDiv_1.innerHTML = "";
+contentDiv_2.innerHTML = "";
+
+window.addEventListener('DOMContentLoaded',function() {
+    const newDiv = paragraphs_1.filter(word => word !== '')
+    newDiv.forEach((paragraph) => {
+    const p = document.createElement("p");
+    p.innerHTML = paragraph;
+    contentDiv_1.appendChild(p);
+    });
+
+    const newDi = paragraphs_2.filter(word => word !== '')
+    newDi.forEach((paragraph) => {
+    const p = document.createElement("p");
+    p.innerHTML = paragraph;
+    contentDiv_2.appendChild(p);
+    });
+})
+
